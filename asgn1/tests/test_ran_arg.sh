dd5674b74106d632b06d61660c1708af00f36e9e
Mira Saini <142550352+Simba4077@users.noreply.github.com>
2024-10-29 21:36:19 -0700

    Create test_ran_arg.sh

---
+++ asgn1/tests/test_ran_arg.sh
@@ -0,0 +1,31 @@
+#Tests for decimal argument: 3.14 and 4
+# Creates the example output
+echo "BAD INPUT" > expected.txt 
+
+# Runs the program
+./calc ! $ > actual.txt 
+
+# Ensures exit code is non-Zero
+if [ $? -eq 0 ]; then
+    echo "invalid exit code" $?
+    rm expected.txt
+    rm actual.txt
+    iexit 1
+fi
+
+# Ensures differences *are* found
+diff actual.txt expected.txt
+if [ $? -ne 0 ]; then
+    echo "Program didn't print BAD INPUT"
+    rm expected.txt
+    rm actual.txt
+    exit 1
+fi
+
+# Prints a message on success
+echo "Detected non-integer characters"
+
+# Cleans up files created
+rm expected.txt
+rm actual.txt
+exit 0
